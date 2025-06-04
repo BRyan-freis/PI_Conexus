839987f2eea95f603e7e94a6192f0074dc12a243
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-06-04 16:51:01 -0300

    Add files via upload

---
+++ pi-conexus/home/home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../home/style.css">
+    <title>Conexus</title>
+</head>
+
+<body>
+    <section class="container">
+        <div id="titulo">
+            <div class="container-header">
+                <h1>Bem-vindo à </h1>
+                <h1 class="conexus">C<span class="string">o</span>nexus</h1>
+                <h3>Porque todo mundo precisa de um lugar para se "conectar"...</h3>
+
+            </div>
+
+            <div class="container-content">
+                <div class="container-text">
+
+                    <p>
+                        A vida pode ser corrida, confusa, cheia de altos e baixos. Às vezes tudo tá bem, às vezes
+                        nem tanto. E tá tudo certo.
+                        A Conexus é pra esses momentos — e pra todos os outros também.
+                    </p>
+                    <p>
+                        Aqui, você se conecta com pessoas que sabem ouvir, que compartilham histórias, que acolhem
+                        sem perguntas estranhas ou olhares tortos.
+                        É um lugar pra conversar de verdade, sem máscaras, sem julgamentos.
+                    </p>
+                    <p>
+                        Não é sobre ser forte o tempo todo. É sobre ser real, humano, e entender que tá tudo bem
+                        sentir.
+                    </p>
+                    <p>
+                        🧠 Entra num grupo, conta o que quiser, ouve o que precisar. 💬 Conhece gente que também tá
+                        buscando leveza, sentido, ou só um respiro.
+                        🗨️ Troca ideia, desabafa, ri, se encontra.
+                    </p>
+                    <p>
+                        Na Conexus, não tem espaço pra zoeira sem noção, piada ofensiva ou gente que entra só pra
+                        atrapalhar.
+                        Aqui o respeito é regra, e quem não sabe conviver com empatia, simplesmente não fica.
+                    </p>
+                    <p>
+                        Porque no fim das contas, todo mundo precisa se conectar... com outras pessoas, consigo
+                        mesmo, com a vida... E é por isso que estamos aqui...
+                    </p>
+                    <p>É por isso que somos a <strong>CONEXUS</strong>.</p>
+                </div>
+
+            </div>
+    </section>
+
+</body>
